c6ea192090650a4e1a6d2098422572805637134a
user <user@user-A6TBFUG>
2019-11-12 21:31:15 +0900

    MySecondCommit

---
+++ src/aws/awsTest.java
@@ -0,0 +1,164 @@
+package aws;
+
+import java.util.Scanner;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.RebootInstancesRequest;
+import com.amazonaws.services.ec2.model.RebootInstancesResult;
+import com.amazonaws.services.ec2.model.Reservation;
+import com.amazonaws.services.ec2.model.StartInstancesRequest;
+import com.amazonaws.services.ec2.model.StopInstancesRequest;
+
+public class awsTest {
+
+static AmazonEC2 ec2;
+
+private static void init() throws Exception {
+
+ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+try {
+    credentialsProvider.getCredentials();
+    } catch (Exception e) {
+        throw new AmazonClientException(
+                        "Cannot load the credentials from the credential profiles file. " +
+                        "Please make sure that your credentials file is at the correct " +
+                        "location (~/.aws/credentials), and is in valid format.",
+                        e);
+        }
+ec2 = AmazonEC2ClientBuilder.standard()
+.withCredentials(credentialsProvider)
+.withRegion("us-east-1") /* check the region at AWS console */
+.build();
+}
+
+
+public static void main(String[] args) throws Exception {
+init();
+Scanner menu = new Scanner(System.in);
+int number = 0;
+
+while(true)
+{
+    System.out.println(" ");
+    System.out.println(" ");
+    System.out.println("------------------------------------------------------------");
+    System.out.println(" Amazon AWS Control Panel using SDK ");
+    System.out.println(" ");
+    System.out.println(" Cloud Computing, Computer Science Department ");
+    System.out.println(" at Chungbuk National University ");
+    System.out.println("------------------------------------------------------------");
+    System.out.println(" 1. list instance 2. available zones ");
+    System.out.println(" 3. start instance 4. available regions ");
+    System.out.println(" 5. stop instance 6. create instance ");
+    System.out.println(" 7. reboot instance 8. list images ");
+    System.out.println(" 99. quit ");
+    System.out.println("------------------------------------------------------------");
+    System.out.print("Enter an integer: ");
+    number = menu.nextInt();
+   
+    switch(number) {
+    case 1 :
+        listInstances();
+        break;
+    case 3 :
+        startInstances();
+        break;
+    case 5 :
+        stopInstances();
+        break;
+    case 7 :
+        rebootInstances();
+        break;
+        }
+    }
+}
+
+
+
+public static void listInstances() {
+System.out.println("Listing instances....");
+boolean done = false;
+
+DescribeInstancesRequest Describe_request = new DescribeInstancesRequest();
+while(!done) {
+    DescribeInstancesResult response = ec2.describeInstances(Describe_request);
+    for(Reservation reservation : response.getReservations()) {
+        for(Instance instance : reservation.getInstances()) {
+            System.out.printf(
+                            "[id] %s, " +
+                            "[AMI] %s, " +
+                            "[type] %s, " +
+                            "[state] %10s, " +
+                            "[monitoring state] %s",
+                            instance.getInstanceId(),
+                            instance.getImageId(),
+                            instance.getInstanceType(),
+                            instance.getState().getName(),
+                            instance.getMonitoring().getState());
+            }
+        System.out.println();
+        }
+    Describe_request.setNextToken(response.getNextToken());
+   
+    if(response.getNextToken() == null) {
+        done = true;
+        }
+    }
+}
+
+
+public static void startInstances() {
+    Scanner scanner = new Scanner(System.in);
+    String instance_id ="";
+   
+    System.out.print("Enter instance id: ");
+    instance_id = scanner.next();
+       
+    StartInstancesRequest Start_request = new StartInstancesRequest()
+                .withInstanceIds(instance_id);
+
+        ec2.startInstances(Start_request);
+
+        System.out.printf("Successfully started instance %s", instance_id);
+}
+
+
+public static void stopInstances() {
+    Scanner scanner = new Scanner(System.in);
+    String instance_id ="";
+   
+    System.out.print("Enter instance id: ");
+    instance_id = scanner.next();
+   
+    final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
+
+    StopInstancesRequest Stop_request = new StopInstancesRequest()
+        .withInstanceIds(instance_id);
+
+    ec2.stopInstances(Stop_request);
+}
+
+
+public static void rebootInstances() {
+    Scanner scanner = new Scanner(System.in);
+    String instance_id ="";
+   
+    System.out.print("Enter instance id: ");
+    instance_id = scanner.next();
+   
+    final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
+
+    RebootInstancesRequest Reboot_request = new RebootInstancesRequest()
+        .withInstanceIds(instance_id);
+
+    RebootInstancesResult response = ec2.rebootInstances(Reboot_request);
+
+    System.out.printf("Successfully rebooted instance %s", instance_id);
+}
+} 
