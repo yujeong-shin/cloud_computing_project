c0232311978acd0eeddefed6265dc78d6c3122ee
user <user@user-A6TBFUG>
2019-11-25 23:25:16 +0900

    MyFourthCommit

--- src/aws/awsTest.java
+++ src/aws/awsTest.java
@@ -1,5 +1,7 @@
 package aws;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.amazonaws.AmazonClientException;
@@ -7,17 +9,27 @@ import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 import com.amazonaws.services.ec2.model.AvailabilityZone;
+import com.amazonaws.services.ec2.model.CreateTagsRequest;
+import com.amazonaws.services.ec2.model.CreateTagsResult;
 import com.amazonaws.services.ec2.model.DescribeAvailabilityZonesResult;
+import com.amazonaws.services.ec2.model.DescribeImagesRequest;
+import com.amazonaws.services.ec2.model.DescribeImagesResult;
 import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
 import com.amazonaws.services.ec2.model.DescribeRegionsResult;
+import com.amazonaws.services.ec2.model.Image;
 import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.InstanceType;
 import com.amazonaws.services.ec2.model.RebootInstancesRequest;
 import com.amazonaws.services.ec2.model.RebootInstancesResult;
 import com.amazonaws.services.ec2.model.Region;
 import com.amazonaws.services.ec2.model.Reservation;
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.RunInstancesResult;
 import com.amazonaws.services.ec2.model.StartInstancesRequest;
 import com.amazonaws.services.ec2.model.StopInstancesRequest;
+import com.amazonaws.services.ec2.model.Tag;
+import com.amazonaws.services.ec2.model.Filter;
 
 public class awsTest {
 
@@ -82,8 +94,14 @@ while(true)
     case 5 :
         stopInstances();
         break;
+    case 6 :
+    	createInstances();
+    	break;
     case 7 :
         rebootInstances();
+        break;
+    case 8 :
+    	listImages();
         break;
         }
     }
@@ -95,10 +113,10 @@ public static void listInstances() {
 System.out.println("Listing instances....");
 boolean done = false;
 
-DescribeInstancesRequest Describe_request = new DescribeInstancesRequest();
+DescribeInstancesRequest DI_request = new DescribeInstancesRequest();
 while(!done) {
-    DescribeInstancesResult response = ec2.describeInstances(Describe_request);
-    for(Reservation reservation : response.getReservations()) {
+    DescribeInstancesResult DI_response = ec2.describeInstances(DI_request);
+    for(Reservation reservation : DI_response.getReservations()) {
         for(Instance instance : reservation.getInstances()) {
             System.out.printf(
                             "[id] %s, " +
@@ -114,9 +132,9 @@ while(!done) {
             }
         System.out.println();
         }
-    Describe_request.setNextToken(response.getNextToken());
+    DI_request.setNextToken(DI_response.getNextToken());
    
-    if(response.getNextToken() == null) {
+    if(DI_response.getNextToken() == null) {
         done = true;
         }
     }
@@ -126,12 +144,12 @@ while(!done) {
 public static void availableZones() {
 	int count = 0;
 	
-    final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
+    //final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
 
-    DescribeAvailabilityZonesResult zones_response = ec2.describeAvailabilityZones();
+    DescribeAvailabilityZonesResult DAZ_response = ec2.describeAvailabilityZones();
 
     System.out.println("Available zones....");
-    for(AvailabilityZone zone : zones_response.getAvailabilityZones()) {
+    for(AvailabilityZone zone : DAZ_response.getAvailabilityZones()) {
         System.out.printf(
         		"[id] %s, " +
                 "[region]   %s, " +
@@ -154,21 +172,21 @@ public static void startInstances() {
     System.out.print("Enter instance id: ");
     instance_id = scanner.next();
        
-    StartInstancesRequest Start_request = new StartInstancesRequest()
+    StartInstancesRequest SI_request = new StartInstancesRequest()
                 .withInstanceIds(instance_id);
 
-        ec2.startInstances(Start_request);
+        ec2.startInstances(SI_request);
 
         System.out.printf("Successfully started instance %s", instance_id);
 }
 
 // 4.
 public static void availableRegions() {
-	final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
+	//final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
 
-    DescribeRegionsResult regions_response = ec2.describeRegions();
+    DescribeRegionsResult DR_response = ec2.describeRegions();
     System.out.println("Available regions....");
-    for(Region region : regions_response.getRegions()) {
+    for(Region region : DR_response.getRegions()) {
         System.out.printf(
             "[region]	%s,	" +
             "[endpoint]	%s",
@@ -188,12 +206,51 @@ public static void stopInstances() {
    
     final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
 
-    StopInstancesRequest Stop_request = new StopInstancesRequest()
+    StopInstancesRequest SI_request = new StopInstancesRequest()
         .withInstanceIds(instance_id);
 
-    ec2.stopInstances(Stop_request);
+    ec2.stopInstances(SI_request);
 }
 
+// 6.
+public static void createInstances() {
+	Scanner scanner = new Scanner(System.in);
+    String ami_id ="";
+    String name = "java-test";
+    
+    System.out.println("Enter ami id: ");
+    ami_id = scanner.next();
+
+    final String USAGE =
+            "To run this example, supply an instance name and AMI image id\n" +
+            "Ex: CreateInstance <instance-name> <ami-image-id>\n";
+
+        final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
+
+        RunInstancesRequest run_request = new RunInstancesRequest()
+            .withImageId(ami_id)
+            .withInstanceType(InstanceType.T1Micro)
+            .withMaxCount(1)
+            .withMinCount(1);
+
+        RunInstancesResult run_response = ec2.runInstances(run_request);
+
+        String reservation_id = run_response.getReservation().getInstances().get(0).getInstanceId();
+
+        Tag tag = new Tag()
+            .withKey("Name")
+            .withValue(name);
+
+        CreateTagsRequest tag_request = new CreateTagsRequest()
+            .withTags(tag);
+
+        CreateTagsResult tag_response = ec2.createTags(tag_request);
+
+        System.out.printf(
+            "Successfully started EC2 instance %s based on AMI %s",
+            reservation_id, ami_id);
+    	
+}
 // 7.
 public static void rebootInstances() {
     Scanner scanner = new Scanner(System.in);
@@ -202,14 +259,46 @@ public static void rebootInstances() {
     System.out.print("Enter instance id: ");
     instance_id = scanner.next();
    
-    final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
+    //final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
 
-    RebootInstancesRequest Reboot_request = new RebootInstancesRequest()
+    RebootInstancesRequest RI_request = new RebootInstancesRequest()
         .withInstanceIds(instance_id);
 
-    RebootInstancesResult response = ec2.rebootInstances(Reboot_request);
+    RebootInstancesResult response = ec2.rebootInstances(RI_request);
 
     System.out.printf("Successfully rebooted instance %s", instance_id);
 }
 
+// 8.
+public static void listImages() {
+	//final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
+	System.out.println("Listing images....");
+	DescribeImagesRequest describeImagesRequest = new DescribeImagesRequest();
+	
+	List<Filter> filters = new ArrayList<>();
+	Filter filter = new Filter();
+	filter.setName("is-public");
+
+	
+	 List<String> values = new ArrayList<>();
+	 values.add("false");
+	 filter.setValues(values);
+	 	 
+	 filters.add(filter);
+	 
+	 describeImagesRequest.setFilters(filters);
+
+	 
+	 DescribeImagesResult describeImagesResult = ec2.describeImages(describeImagesRequest);	
+	 for(Image images :describeImagesResult.getImages()){
+			System.out.printf("[ImageID] %s,	" +
+	        					"[Name] %s,	" +
+	        					"[Owner] %s",
+	        					images.getImageId(),
+	        					images.getName(),
+	        					images.getOwnerId());
+	    }
+	 
+	
+    }
 } 
